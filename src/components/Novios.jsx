e149c7f670bd4880c658d0d44b749c7383e39f93
Jhoel Moreno <jhoel.moreno24@gmail.com>
2022-07-25 12:18:32 -0600

    seccion novios terminada

--- src/components/Novios.jsx
+++ src/components/Novios.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import borderImage from '../assets/images/borde-titulo.png'
+import parejaFeliz from '../assets/images/pareja-feliz.webp'
 
 const Titulo = styled.h2`
     font-family: 'Great Vibes', cursive;
@@ -24,16 +25,39 @@ const Titulo = styled.h2`
         }
     }
 `
+const ContenedorImagenTexto = styled.div`
+    margin: 1rem 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 3rem;
 
+    @media (max-width: 768px){
+        flex-direction: column;
+        gap: 2rem;
+    }
+
+    .imagen, .texto {
+        flex: 1
+    }
+`
 
 
 const Novios = () => {
     return (
         <>
-                <Titulo>
-                    Novios
-                        <img src={borderImage} alt="borde" />
-                </Titulo>
+            <Titulo>
+                Algún texto bonito
+                    <img src={borderImage} alt="borde" />
+            </Titulo>
+            <ContenedorImagenTexto>
+                <div className='imagen'>
+                    <img src={parejaFeliz} alt="Pareja feliz" />
+                </div>
+                <div className='texto'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi fugiat enim omnis rem quo suscipit, sed expedita id voluptas minima nulla in praesentium recusandae iusto! Quam voluptatibus quis consectetur dicta?</p>
+                </div>
+            </ContenedorImagenTexto>
         </>
     )
 }
