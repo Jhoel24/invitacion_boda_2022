434d759323be16e81d4df6e620c77a32f0bc77f8
Jhoel Moreno <jhoel.moreno24@gmail.com>
2022-07-24 23:30:48 -0600

    Galeria solucionada

--- src/components/ContenedorImagen.jsx
+++
@@ -1,9 +0,0 @@
-const ContenedorImagen = ({n}) => {
-    return (
-        <div>
-            <img src={`./assets/images/galeria-imagen${n}.jpeg`} alt={`Imagen no ${n}`} />
-        </div>
-    )
-}
-
-export default ContenedorImagen
--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
-import ContenedorImagen from './ContenedorImagen'
+import imagen1 from '../assets/images/galeria-imagen1.jpeg'
+import imagen2 from '../assets/images/galeria-imagen2.jpeg'
+import imagen3 from '../assets/images/galeria-imagen3.jpeg'
+import imagen4 from '../assets/images/galeria-imagen4.jpeg'
+import imagen5 from '../assets/images/galeria-imagen5.jpeg'
+import imagen6 from '../assets/images/galeria-imagen6.jpeg'
+import imagen7 from '../assets/images/galeria-imagen7.jpeg'
+import imagen8 from '../assets/images/galeria-imagen8.jpeg'
+import imagen9 from '../assets/images/galeria-imagen9.jpeg'
+import imagen10 from '../assets/images/galeria-imagen10.jpeg'
+
 
 const Titulo = styled.h1`
     font-family: 'Great Vibes', cursive;
@@ -15,18 +25,40 @@ const Galeria = styled.div`
 
 const GalleryComponent = () => {
 
-    const arrNumeroImagenes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     return (
         <div className='galeria'>
             <Titulo>Galería</Titulo>
             <Galeria>
-                {arrNumeroImagenes.map(n => (
-                    <ContenedorImagen 
-                        key={n}
-                        n={n}
-                    />
-                ))}
+                <div>
+                    <img src={imagen1} alt="Imagen 1" />
+                </div>
+                <div>
+                    <img src={imagen2} alt="Imagen 2" />
+                </div>
+                <div>
+                    <img src={imagen3} alt="Imagen 3" />
+                </div>
+                <div>
+                    <img src={imagen4} alt="Imagen 4" />
+                </div>
+                <div>
+                    <img src={imagen5} alt="Imagen 5" />
+                </div>
+                <div>
+                    <img src={imagen6} alt="Imagen 6" />
+                </div>
+                <div>
+                    <img src={imagen7} alt="Imagen 7" />
+                </div>
+                <div>
+                    <img src={imagen8} alt="Imagen 8" />
+                </div>
+                <div>
+                    <img src={imagen9} alt="Imagen 9" />
+                </div>
+                <div>
+                    <img src={imagen10} alt="Imagen 10" />
+                </div>
             </Galeria>
         </div>
     )
